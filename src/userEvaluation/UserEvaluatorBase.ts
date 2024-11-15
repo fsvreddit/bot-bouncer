cb04218cc2b0f4dc528a6f26b1ba1cb04ea037d9
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2024-11-15 18:59:04 +0000

    Change method to abstract

--- src/userEvaluation/UserEvaluatorBase.ts
+++ src/userEvaluation/UserEvaluatorBase.ts
@@ -10,8 +10,5 @@ export abstract class UserEvaluatorBase {
         this.context = context;
     }
 
-    // eslint-disable-next-line @typescript-eslint/require-await
-    async evaluate (): Promise<boolean> {
-        throw new Error("Not implemented on base class");
-    }
+    abstract evaluate (): Promise<boolean>;
 }
