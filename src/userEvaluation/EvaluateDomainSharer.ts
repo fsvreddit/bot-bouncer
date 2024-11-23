dcd577f1f36f79c34c314d34e2d84130d6624c6f
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2024-11-23 11:19:07 +0000

    Fix crash checking URLs on internal reddit links

--- src/userEvaluation/EvaluateDomainSharer.ts
+++ src/userEvaluation/EvaluateDomainSharer.ts
@@ -11,6 +11,11 @@ export class EvaluateDomainSharer extends UserEvaluatorBase {
     };
 
     private domainFromUrl (url: string): string | undefined {
+        if (url.startsWith("/")) {
+            // Reddit internal link
+            return;
+        }
+
         const hostname = new URL(url).hostname;
         const trimmedHostname = hostname.startsWith("www.") ? hostname.substring(4) : hostname;
         if (trimmedHostname === "reddit.com" || trimmedHostname === "i.redd.it" || trimmedHostname === "v.redd.it") {
