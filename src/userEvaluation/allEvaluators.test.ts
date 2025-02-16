b3a062348f5655109504932dcdf42fa47830a5bb
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-02-16 09:43:03 +0000

    Add test to ensure unique evaluator names

---
+++ src/userEvaluation/allEvaluators.test.ts
@@ -0,0 +1,11 @@
+import { ALL_EVALUATORS } from "./allEvaluators.js";
+
+test("All evaluators have a unique name", () => {
+    const evaluatorNames = new Set<string>();
+    for (const Evaluator of ALL_EVALUATORS) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+        const evaluator = new Evaluator({} as any, {});
+        expect(evaluatorNames.has(evaluator.name)).toBe(false);
+        evaluatorNames.add(evaluator.name);
+    }
+});
