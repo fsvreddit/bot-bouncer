90bcb41fb69f91b29898fbeae48ab68b7a6d84dd
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-03-16 16:02:30 +0000

    Update comment to reflect code

--- src/handleClientSubredditWikiUpdate.ts
+++ src/handleClientSubredditWikiUpdate.ts
@@ -157,7 +157,7 @@ async function handleSetBanned (username: string, subredditName: string, setting
 
     if (removableContent.length > 0) {
         await context.redis.hSet(`removedItems:${username}`, fromPairs(removableContent.map(item => ([item.id, item.id]))));
-        // Expire key after 28 days
+        // Expire key after 14 days
         await context.redis.expire(`removedItems:${username}`, 60 * 60 * 24 * 14);
     }
 }
