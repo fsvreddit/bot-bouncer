2fed917ed725fe62cc24822b5b6a78c4eac05cb3
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-03-22 09:33:53 +0000

    Add test to ensure all evaluators have a unique killswitch name

--- src/userEvaluation/allEvaluators.test.ts
+++ src/userEvaluation/allEvaluators.test.ts
@@ -9,3 +9,13 @@ test("All evaluators have a unique name", () => {
         evaluatorNames.add(evaluator.name);
     }
 });
+
+test("All evaluators have a unique killswitch name", () => {
+    const evaluatorKillswitches = new Set<string>();
+    for (const Evaluator of ALL_EVALUATORS) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+        const evaluator = new Evaluator({} as any, {});
+        expect(evaluatorKillswitches.has(evaluator.killswitch)).toBe(false);
+        evaluatorKillswitches.add(evaluator.name);
+    }
+});
