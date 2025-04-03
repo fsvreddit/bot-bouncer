7d9cbeddd46bfc77b8c97a913c9994357e7adda6
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-04-03 13:29:54 +0100

    Fix broken test

--- src/userEvaluation/EvaluateSelfComment.test.ts
+++ src/userEvaluation/EvaluateSelfComment.test.ts
@@ -20,12 +20,14 @@ test("User with a self-comment", () => {
             id: "t1_fake",
             authorId: "t2_fake",
             parentId: "t3_fake2",
+            subredditName: "FunnyVideos",
             body: "This is a test comment.",
         } as unknown as Comment,
         {
             createdAt: subMinutes(new Date(), 1),
             id: "t3_fake2",
             authorId: "t2_fake",
+            subredditName: "FunnyVideos",
             url: "https://i.redd.it/abc123",
         } as unknown as Post,
     ];
@@ -42,12 +44,14 @@ test("User with a comment on someone else's post", () => {
             id: "t1_fake",
             authorId: "t2_fake",
             parentId: "t3_fake",
+            subredditName: "FunnyVideos",
             body: "This is a test comment.",
         } as unknown as Comment,
         {
             createdAt: subMinutes(new Date(), 1),
             id: "t3_fake2",
             authorId: "t2_fake2",
+            subredditName: "FunnyVideos",
             url: "https://i.redd.it/abc123",
         } as unknown as Post,
     ];
