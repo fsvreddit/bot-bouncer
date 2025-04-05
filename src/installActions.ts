8af08bde6c2c8b1c2918d5a3804cfd2f078b0391
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-04-05 08:11:59 +0100

    Remove obsolete entry from KarmaFarmingSubsCheckDates during control subreddit job setup

--- src/installActions.ts
+++ src/installActions.ts
@@ -62,8 +62,6 @@ async function addControlSubredditJobs (context: TriggerContext) {
     await handleExternalSubmissionsPageUpdate(context);
     await removeRetiredEvaluatorsFromStats(context);
 
-    await context.redis.zRem("KarmaFarmingSubsCheckDates", ["WhatIsMyCQS"]);
-
     console.log("App Install: Control subreddit jobs added");
 }
 
