9e5359124e337718a759fbac90cf04aebbc2bfd2
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-04-13 16:04:57 +0100

    Update version in YAML to 1.7.214 and fix user status check in activity validation

--- src/activityHistory.ts
+++ src/activityHistory.ts
@@ -53,7 +53,7 @@ interface ActivityRecord {
 
 async function checkUserActivity (username: string, context: JobContext) {
     const currentStatus = await getUserStatus(username, context);
-    if (currentStatus?.lastStatus !== UserStatus.Banned) {
+    if (currentStatus?.userStatus !== UserStatus.Banned) {
         await removeActivityCheckRecords(username, context);
         console.log(`Activity Check: User ${username} is no longer banned. Removing from activity check queue.`);
         return;
