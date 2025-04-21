e060d4500bbfd311e4ec71e53c1d375aa1c8b2f0
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-04-21 15:07:33 +0100

    Add Advice Bot evaluator

--- src/userEvaluation/allEvaluators.ts
+++ src/userEvaluation/allEvaluators.ts
@@ -23,6 +23,7 @@ import { EvaluateInconsistentGenderBot } from "./EvaluateInconsistentGenderBot.j
 import { EvaluateOFLinksBot } from "./EvaluateOFLinksBot.js";
 import { EvaluateBadDisplayName } from "./EvaluateBadDisplayname.js";
 import { EvaluateSequencePostBot } from "./EvaluateSequencePostBot.js";
+import { EvaluateAdviceBot } from "./EvaluateAdviceBot.js";
 
 export const ALL_EVALUATORS = [
     EvaluateBadUsername,
@@ -50,4 +51,5 @@ export const ALL_EVALUATORS = [
     EvaluateOFLinksBot,
     EvaluateBadDisplayName,
     EvaluateSequencePostBot,
+    EvaluateAdviceBot,
 ];
