3a2bac7d7e32f6c426f0cea5ba302977f36acdab
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-04-25 08:33:07 +0100

    json2md changes

--- src/handleControlSubSubmission.ts
+++ src/handleControlSubSubmission.ts
@@ -7,6 +7,7 @@ import { subMonths } from "date-fns";
 import { getControlSubSettings } from "./settings.js";
 import { createNewSubmission } from "./postCreation.js";
 import { getUserExtended, UserExtended } from "./extendedDevvit.js";
+import json2md from "json2md";
 
 export async function handleControlSubSubmission (event: PostCreate, context: TriggerContext) {
     if (context.subredditName !== CONTROL_SUBREDDIT) {
@@ -24,7 +25,7 @@ export async function handleControlSubSubmission (event: PostCreate, context: Tr
         return;
     }
 
-    let submissionResponse: string | undefined;
+    const submissionResponse: json2md.DataObject[] = [];
 
     const username = getUsernameFromUrl(event.post.url);
 
@@ -34,17 +35,20 @@ export async function handleControlSubSubmission (event: PostCreate, context: Tr
             return;
         }
 
-        submissionResponse = "Hi, thanks for your submission.\n\nOnly links to user accounts are permitted here.";
+        submissionResponse.push(
+            { p: "Hi, thanks for your submission" },
+            { p: "Only links to user accounts are permitted here." },
+        );
     }
 
     const controlSubSettings = await getControlSubSettings(context);
     if (controlSubSettings.reporterBlacklist.includes(event.author.name)) {
-        submissionResponse = "You are not currently permitted to submit bots to r/BotBouncer. Please write in to modmail if you believe this is a mistake";
+        submissionResponse.push({ p: "You are not currently permitted to submit bots to r/BotBouncer. Please write in to modmail if you believe this is a mistake" });
     }
 
     const submitterStatus = await getUserStatus(event.author.name, context);
     if (submitterStatus && submitterStatus.userStatus === UserStatus.Banned) {
-        submissionResponse = "You are currently listed as a bot on r/BotBouncer, so we cannot accept submissions from you. Please write in to modmail if you believe this is a mistake";
+        submissionResponse.push({ p: "You are currently listed as a bot on r/BotBouncer, so we cannot accept submissions from you. Please write in to modmail if you believe this is a mistake" });
     }
 
     let user: UserExtended | undefined;
@@ -52,12 +56,18 @@ export async function handleControlSubSubmission (event: PostCreate, context: Tr
         user = await getUserExtended(username, context);
     }
 
-    if (!user && !submissionResponse) {
-        submissionResponse = `Hi, thanks for your submission.\n\n${username} appears to be deleted, suspended or shadowbanned already, so no post will be created for it.`;
+    if (!user && submissionResponse.length === 0) {
+        submissionResponse.push(
+            { p: "Hi, thanks for your submission" },
+            { p: `${username} appears to be deleted, suspended or shadowbanned already, so no post will be created for it.` },
+        );
     }
 
     if (user?.username === event.author.name) {
-        submissionResponse = "Hi, thanks for your submission.\n\nYou cannot make a submission for yourself.";
+        submissionResponse.push(
+            { p: "Hi, thanks for your submission" },
+            { p: "You cannot make a submission for yourself." },
+        );
     }
 
     if (user) {
@@ -68,17 +78,24 @@ export async function handleControlSubSubmission (event: PostCreate, context: Tr
         }).all();
 
         if (userContent.filter(item => item.createdAt > subMonths(new Date(), controlSubSettings.maxInactivityMonths ?? 3)).length === 0) {
-            submissionResponse = `Hi, thanks for your submission.\n\n${username} has no recent content on their history, so may be retired. Submissions can only be made for active users.`;
+            submissionResponse.push(
+                { p: "Hi, thanks for your submission" },
+                { p: `${username} has no recent content on their history, so may be retired. Submissions can only be made for active users.` },
+            );
         }
     }
 
-    if (user && !submissionResponse) {
+    if (user && submissionResponse.length === 0) {
         const currentStatus = await getUserStatus(user.username, context);
         if (currentStatus) {
             const post = await context.reddit.getPostById(currentStatus.trackingPostId);
-            submissionResponse = `Hi, thanks for your submission.\n\n${username} is already tracked by Bot Bouncer with a current status of ${currentStatus.userStatus}, you can see the submission [here](${post.permalink}).`;
+            submissionResponse.push(
+                { p: "Hi, thanks for your submission" },
+                { p: `${username} is already tracked by Bot Bouncer with a current status of ${currentStatus.userStatus}, you can see the submission [here](${post.permalink}).` },
+            );
+
             if (currentStatus.userStatus === UserStatus.Organic) {
-                submissionResponse += `\n\nIf you have information about how this user is a bot that we may have missed, please [modmail us](https://www.reddit.com/message/compose?to=/r/BotBouncer&subject=More%20information%20about%20/u/${user.username}) with the details, so that we can review again.`;
+                submissionResponse.push({ p: `If you have information about how this user is a bot that we may have missed, please [modmail us](https://www.reddit.com/message/compose?to=/r/BotBouncer&subject=More%20information%20about%20/u/${user.username}) with the details, so that we can review again.` });
             }
         } else {
             const newStatus = controlSubSettings.trustedSubmitters.includes(event.author.name) ? UserStatus.Banned : UserStatus.Pending;
@@ -95,7 +112,10 @@ export async function handleControlSubSubmission (event: PostCreate, context: Tr
 
             console.log(`Created new post for ${username}`);
 
-            submissionResponse = `Hi, thanks for your submission.\n\nThe post tracking ${user.username} can be found [here](${newPost.permalink}).\n\nYour post has been removed, and can be deleted.`;
+            submissionResponse.push(
+                { p: "Hi, thanks for your submission" },
+                { p: `The post tracking ${user.username} can be found [here](${newPost.permalink}).\n\nYour post has been removed, and can be deleted.` },
+            );
 
             await context.scheduler.runJob({
                 name: ControlSubredditJob.EvaluateUser,
@@ -108,10 +128,10 @@ export async function handleControlSubSubmission (event: PostCreate, context: Tr
         }
     }
 
-    if (submissionResponse) {
+    if (submissionResponse.length > 0) {
         const newComment = await context.reddit.submitComment({
             id: event.post.id,
-            text: submissionResponse,
+            text: json2md(submissionResponse),
         });
         await newComment.distinguish(true);
     }
