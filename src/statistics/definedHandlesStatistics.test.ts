98579a83167cb3b88f83d81e906b3789db265008
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-11-04 08:28:06 +0000

    Upgrade ESLint and remove obsolete test

--- src/statistics/definedHandlesStatistics.test.ts
+++
@@ -1,8 +0,0 @@
-import { getHandlesFromRegex } from "./definedHandlesStatistics.js";
-
-test("Defined Handles Splitting", () => {
-    const input = "handle1|(?:handle2|handle2)|handle3";
-    const expected = ["handle1", "(?:handle2|handle2)", "handle3"];
-    const result = getHandlesFromRegex(input);
-    expect(result).toEqual(expected);
-});
