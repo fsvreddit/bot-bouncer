e7147f164fe003d570d33cd6185891b96d8691df
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-11-07 11:32:48 +0000

    Refactor rebalanceCohorts function to adjust account movement logic and remove redundant rebalance key check

--- src/karmaFarmingSubsCheck.ts
+++ src/karmaFarmingSubsCheck.ts
@@ -5,7 +5,7 @@ import { CONTROL_SUBREDDIT, ControlSubredditJob } from "./constants.js";
 import { getAllKnownUsers, getUserStatus, UserDetails, UserStatus } from "./dataStore.js";
 import { evaluateUserAccount, storeAccountInitialEvaluationResults, userHasContinuousNSFWHistory } from "./handleControlSubAccountEvaluation.js";
 import { getControlSubSettings } from "./settings.js";
-import { addMinutes, addSeconds, differenceInMinutes, subMinutes, subWeeks } from "date-fns";
+import { addSeconds, differenceInMinutes, subMinutes, subWeeks } from "date-fns";
 import { getUserExtended } from "./extendedDevvit.js";
 import { AsyncSubmission, PostCreationQueueResult, queuePostCreation } from "./postCreation.js";
 import pluralize from "pluralize";
@@ -184,29 +184,25 @@ export async function queueKarmaFarmingSubs (_: unknown, context: JobContext) {
 }
 
 async function rebalanceCohorts (context: JobContext) {
-    const rebalanceDoneKey = "KarmaFarmingSubsCohortRebalanceDone";
-    if (await context.redis.exists(rebalanceDoneKey)) {
-        return;
-    }
-    await context.redis.set(rebalanceDoneKey, "true", { expiration: addMinutes(new Date(), 10) });
-
     const allAccounts = await context.redis.global.zRange(ACCOUNTS_QUEUED_KEY, 0, -1);
     const evenAccounts = allAccounts.filter(item => item.score % 2 === 0);
     const oddAccounts = allAccounts.filter(item => item.score % 2 !== 0);
 
     const difference = evenAccounts.length - oddAccounts.length;
-    if (Math.abs(difference) <= 50) {
+    if (Math.abs(difference) <= 50 || allAccounts.length < 100) {
         return;
     }
 
     let accountsToMove: ZMember[];
     if (difference > 0) {
-        accountsToMove = evenAccounts.slice(0, Math.floor(difference / 2));
+        // Move from evens to odds
+        accountsToMove = evenAccounts.slice(Math.floor(difference / 2));
     } else {
-        accountsToMove = oddAccounts.slice(0, Math.floor(-difference / 2));
+        // Move from odds to evens
+        accountsToMove = oddAccounts.slice(Math.floor(-difference / 2));
     }
 
-    await context.redis.zAdd(ACCOUNTS_QUEUED_KEY, ...accountsToMove.map(item => ({ member: item.member, score: item.score + 1 })));
+    await context.redis.global.zAdd(ACCOUNTS_QUEUED_KEY, ...accountsToMove.map(item => ({ member: item.member, score: item.score + 1 })));
     console.log(`Karma Farming Subs: Rebalanced cohorts by moving ${accountsToMove.length} ${pluralize("account", accountsToMove.length)}`);
 }
 
