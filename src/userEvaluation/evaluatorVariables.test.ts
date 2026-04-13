c615475c64ca1360fc26eef7596fcbf25d9667f1
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-04-13 15:02:42 +0100

    feat: update invalidEvaluatorVariableCondition calls to include test parameter for improved validation

--- src/userEvaluation/evaluatorVariables.test.ts
+++ src/userEvaluation/evaluatorVariables.test.ts
@@ -55,7 +55,7 @@ test("Invalid regex", async () => {
         ],
     };
 
-    const result = await invalidEvaluatorVariableCondition(variables, {} as unknown as JobContext);
+    const result = await invalidEvaluatorVariableCondition(variables, {} as unknown as JobContext, true);
     expect(result.length).toBe(1);
 });
 
@@ -66,7 +66,7 @@ test("Regex with || condition", async () => {
         ],
     };
 
-    const result = await invalidEvaluatorVariableCondition(variables, {} as unknown as JobContext);
+    const result = await invalidEvaluatorVariableCondition(variables, {} as unknown as JobContext, true);
     expect(result.length).toBe(1);
     console.log(result);
 });
@@ -82,7 +82,7 @@ test("Regex with badly formed array", async () => {
         ],
     };
 
-    const result = await invalidEvaluatorVariableCondition(variables, {} as unknown as JobContext);
+    const result = await invalidEvaluatorVariableCondition(variables, {} as unknown as JobContext, true);
     expect(result.length).toBe(1);
 });
 
