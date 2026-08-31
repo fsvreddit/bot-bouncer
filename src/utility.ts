cf8c9b32f36aeae2c58e242694bc91e5e6c29fd1
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-08-31 11:54:44 +0100

    fix: include seconds in duration formatting for improved time accuracy

--- src/utility.ts
+++ src/utility.ts
@@ -202,7 +202,7 @@ export function conditionallyDecompressString (input: string): string {
 
 export function formatTimeSince (date: Date): string {
     const interval = intervalToDuration({ start: date, end: new Date() });
-    return formatDuration(interval, { format: ["days", "hours", "minutes"] });
+    return formatDuration(interval, { format: ["days", "hours", "minutes", "seconds"] });
 }
 
 export function normaliseHitReason (hitReason: HitReason): HitReasonDetailed {
